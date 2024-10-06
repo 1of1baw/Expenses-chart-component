88e646e79b550f74d290f4e9f4b1527eb6c1d0c3
1of1baw <167240062+1of1baw@users.noreply.github.com>
2024-10-06 14:56:17 +0200

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+const days = document.querySelectorAll('.day');
+const dayAmounts = document.querySelectorAll('.day-amount');
+const dayExpenses = document.querySelectorAll('.day-expenses');
+
+const daysOfWeek = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+const currentDate = new Date();
+const dayIndex = currentDate.getDay();
+let today = daysOfWeek[dayIndex];
+
+for (let i = 0; i < daysOfWeek.length; i++) {
+  if (today === days[i].id) {
+    dayExpenses[i].classList.add('active')
+  }
+}
+
+function fetchData() {
+  fetch('data.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+
+      for (let i = 0; i < days.length; i++) {
+
+        if (days[i].id === data[i].day) {
+          dayAmounts[i].innerText = '$' + data[i].amount;
+          dayExpenses[i].style.height = Math.floor(data[i].amount) * 2.9 + 'px'
+        }
+      }
+
+    })
+    .catch(error => {
+      console.error('There was a problem fetching the data:', error);
+    });
+
+}
+
+setTimeout(fetchData,500)
